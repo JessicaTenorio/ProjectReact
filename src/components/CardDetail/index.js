c28d81b97fe339f3f900b59620a2f5f6c2a4d21e
jessica tenorio <j3k.tenorio@gmail.com>
2020-07-31 22:17:39 -0500

    estilo card detail

--- src/components/CardDetail/index.js
+++ src/components/CardDetail/index.js
@@ -16,24 +16,32 @@ class CardDetail extends React.Component {
         const { item } = this.props.location;
 
         return (
-            <div className="CardDetail-div">
-                <div><Link to={'/character'}>
-                    <button>Go back</button>
-                </Link></div>
-                <p>Detail {charId}  </p>
-                <img src={item.image} alt={item.name} width="60" height="60"></img>
-                <p>Name: {item.name}</p>
-                <p>Status: {item.status}</p>
-                <p>Gender: {item.gender}</p>
-                <p>Species: {item.species}</p>
-                <p>Location: {item.location.name}</p>
+            <div className="CardDetail-container">
+                <div className="CardDetail-div">
+
+                    <div>
+                        <div><Link to={'/character'}>
+                            <button>Go back</button>
+                        </Link></div>
+                        <img src={item.image} alt={item.name} width="300" height="300"></img>
+                    </div>
+
+                    <div className="CardDetail-info">
+                        <p>Id: {charId}  </p>
+                        <p>Name: {item.name}</p>
+                        <p>Status: {item.status}</p>
+                        <p>Gender: {item.gender}</p>
+                        <p>Species: {item.species}</p>
+                        <p>Location: {item.location.name}</p>
+                    </div>
+
+                </div>
+
             </div>
+
         )
     };
 
 }
 
 export default CardDetail;
-
-
-
